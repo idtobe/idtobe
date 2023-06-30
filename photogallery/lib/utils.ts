4771ac46cd7df52ae5a5a66fc43cff1f0934ea7b
idtobe <idtobe@gmail.com>
2023-06-30 23:27:06 +0100

    new ui

--- photogallery/lib/utils.ts
+++
@@ -1,4 +0,0 @@
-import parseDate from "date-fns/parse";
-
-export const parseDateTimeOriginal = (dateTimeOriginal: string) =>
-  parseDate(dateTimeOriginal, "yyyy:MM:dd HH:mm:ss", new Date(0)).getTime();
